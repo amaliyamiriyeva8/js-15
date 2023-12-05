4c6ad1a79d9c49f22c840b5e507a28abb871320e
amaliyamiriyeva8 <amaliyamiriyeva67@gmail.com>
2023-12-05 23:41:18 +0400

    homePage

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const section_3=document.querySelector(".toy")
+const toy=document.querySelector(".toy")
 
 fetch(" http://localhost:3000/robots")
 .then(res=>res.json())
 .then(data=>{
     data.forEach(element => {
-        section_3.innerHTML+=`
+        toy.innerHTML+=`
         <div class="toy-1">
         <div class="photo"><img src="${element.image}" alt=""></div>
         <div class="word">
             <h1>${element.name}</h1>
             <p>${element.description}</p>
-            <button><a href="./details.html?id=${element.id}">VIEW DETAILS</a></button>
+            <div class="button"><a href="./details.html?id=${element.id}">VIEW DETAILS</a></div>
         </div>
         </div>
         `
