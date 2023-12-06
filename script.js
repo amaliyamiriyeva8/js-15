486ab42352659dfd6cc3043962a2d86116b6c680
amaliyamiriyeva8 <amaliyamiriyeva67@gmail.com>
2023-12-06 19:50:54 +0400

    none

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+const navbar=document.querySelector("#nav")
+function nvbr() {
+    if (window.scrollY > 35) {
+        navbar.style.top = "0px"
+        navbar.style.backgroundColor = "blueviolet";
+        navbar.style.transition = "all 1s ease";
+    }
+    else {
+        navbar.style.backgroundColor = "";
+        navbar.style.transition = "all 1s ease";
+        navbar.style.top = "";
+    }
+
+}
+window.addEventListener("scroll", nvbr);
 
 
 
-const toy=document.querySelector(".toy")
 
 fetch(" http://localhost:3000/robots")
 .then(res=>res.json())
@@ -24,3 +38,18 @@ function deleteRobot(id){
     axios.delete(`http://localhost:3000/robots/${id}`);
     window.location.reload();
 }
+
+const names=document.querySelector("#fixed");
+console.log(names)
+names.addEventListener("click",()=>{
+    window.scrollTo({
+        top: 0,
+        transition:".9s ease-in",
+        left: 0,
+        behavior: 'smooth',
+        transition: "all 0.3s ease",
+        duration:1
+    });
+
+})
+
