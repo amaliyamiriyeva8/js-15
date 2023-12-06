40cf09dda9710fa642ef26610dff920537410e5f
amaliyamiriyeva8 <amaliyamiriyeva67@gmail.com>
2023-12-07 00:01:10 +0400

    commit-1

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-const navbar=document.querySelector("#nav")
-function nvbr() {
-    if (window.scrollY > 35) {
-        navbar.style.top = "0px"
-        navbar.style.backgroundColor = "blueviolet";
-        navbar.style.transition = "all 1s ease";
-    }
-    else {
-        navbar.style.backgroundColor = "";
-        navbar.style.transition = "all 1s ease";
-        navbar.style.top = "";
-    }
-
-}
-window.addEventListener("scroll", nvbr);
-
-
-
+const toy=document.querySelector(".toy")
 
 fetch(" http://localhost:3000/robots")
 .then(res=>res.json())
@@ -44,7 +27,6 @@ console.log(names)
 names.addEventListener("click",()=>{
     window.scrollTo({
         top: 0,
-        transition:".9s ease-in",
         left: 0,
         behavior: 'smooth',
         transition: "all 0.3s ease",
@@ -52,4 +34,41 @@ names.addEventListener("click",()=>{
     });
 
 })
+const navbar=document.querySelector("#nav")
+function nvbr() {
+    if (window.scrollY > 35) {
+        navbar.style.top = "0px"
+        navbar.style.backgroundColor = "rgba(138, 144, 255, 0.9)";
+        navbar.style.transition = "all 1s ease";
+    }
+    else {
+        navbar.style.backgroundColor = "";
+        navbar.style.transition = "all 1s ease";
+        navbar.style.top = "";
+    }
 
+}
+window.addEventListener("scroll", nvbr);
+
+
+
+let x=document.querySelector("#icon")
+let was=document.querySelector(".was");
+let icon=document.querySelector(".icon-none");
+
+
+icon.addEventListener("click",()=>{
+    was.style.display="flex";
+    icon.style.display="none"
+
+})
+
+x.addEventListener("click",()=>{
+    was.style.display="none"
+    icon.style.display="flex";
+})
+was.addEventListener("click",(e)=>{
+    if(e.target == was){
+        was.style.display="none"
+    }
+})
